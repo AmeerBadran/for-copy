8d0b5b6c1490ace6a35e5af50d15e4c249d4df3c
ameer <abadran281@gmail.com>
2024-12-11 13:21:18 +0200

    contact us page 1/2 done

--- src/router/routers.jsx
+++ src/router/routers.jsx
@@ -7,15 +7,9 @@ import RootLayout from "../components/HOC/RootLayout";
 import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import Contact from "../pages/Contact";
-//import PersistLogin from "../components/HOC/PersistLogin";
-//import NotProtectdRoute from "../components/HOC/withNotProtect"
-//import ProtectdRoute from "../components/HOC/withProtect"
 
 const router = createBrowserRouter([
   {
-    // element: <PersistLogin />,
-    //children: [
-    //{
     path: "/",
     element: <RootLayout />,
     children: [
@@ -33,74 +27,6 @@ const router = createBrowserRouter([
       }
     ],
   },
-  /*{
-    path: "/admin",
-    element: (
-      <ProtectdRoute path="/admin" element={<Admin />} />
-    ),
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "settings",
-        element: <Settings />,
-      },
-      {
-        path: "addEvent",
-        element: <AddEvent />,
-      },
-      {
-        path: "allEvents",
-        element: <AllEvents />,
-      },
-      {
-        path: "allEvents/editEvent",
-        element: (<EditEvent />),
-      },
-      {
-        path: "addUser",
-        element: <AddUser />,
-      },
-
-    ],
-  },*/
-
-  // {
-  //   path: "logIn",
-  //   element: <NotProtectdRoute element={<LogIn />} />,
-  //   children: [
-  //     {
-  //       index: true,
-  //       element: <LoginForm />,
-  //     },
-  //     {
-  //       path: "forgot-password",
-  //       element: <ForgotPasswordForm />,
-  //     },
-  //     {
-  //       path: "reset-password",
-  //       element: <ResetPasswordForm />,
-  //     }
-  //   ]
-  // },
-  //{
-  //  path: "signUp",
-  //  element: (<NotProtectdRoute path="signUp" element={<SignUp />} />),
-  //},
-  //],
-  //},
-  /*
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-    {
-      path: "/server-error",
-      element: <ServerError />,
-    },
-    */
 ],
   {
     basename: "/Twwir/",
